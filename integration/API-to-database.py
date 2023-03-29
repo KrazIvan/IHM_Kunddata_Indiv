65f0310242220b8b27ce764e8b027e272e3002c2
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-03-29 17:21:59 +0200

    Update API-to-database.py

--- integration/API-to-database.py
+++ integration/API-to-database.py
@@ -1,7 +1,74 @@
-from kunddata_imports import få_hubspot_companies, få_hubspot_deals, få_hubspot_kunder, få_newsletters_kunder, få_payments, få_credits_kunder
+from kunddata_imports import få_hubspot_deals, få_hubspot_companies, få_hubspot_kunder, få_newsletters_kunder, få_payments, få_credits_kunder
 import mariadb
+from datetime import datetime
 import sys
 
+# Hubspot -----------------------------------------------------------
+
+def import_hubspot_deals(hubspot_deals):
+    try:
+        conn = mariadb.connect(
+        user="root",
+        password="i",
+        host="localhost",
+        database="hubspot_deals")
+    except mariadb.Error as fel:
+        print(f"Error connecting to MariaDB: {fel}")
+        sys.exit(1)
+
+    cur = conn.cursor()
+
+    cur.execute("CREATE DATABASE IF NOT EXISTS hubspot_deals")
+    cur.execute("USE hubspot_deals")
+    cur.execute("""CREATE TABLE IF NOT EXISTS deals (id BIGINT PRIMARY KEY, 
+    dealname VARCHAR(255), 
+    amount INT,
+    createdate VARCHAR(255),
+    closedate VARCHAR(255),
+    dealstage VARCHAR(255), 
+    pipeline VARCHAR(255))""")
+
+    for deal in hubspot_deals:
+        id = int(deal["id"])
+        amount = int(deal["properties"]["amount"])
+        createdate = deal["properties"]["createdate"]
+        closedate = deal["properties"]["closedate"]
+        dealname = deal["properties"]["dealname"]
+        dealstage = deal["properties"]["dealstage"]
+        pipeline = deal["properties"]["pipeline"]
+
+        cur.execute("INSERT INTO deals (id, dealname, amount, createdate, closedate, dealstage, pipeline) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                    (id, dealname, amount, createdate, closedate, dealstage, pipeline))
+
+    conn.commit()
+    conn.close()
+
+def import_hubspot_companies(hubspot_companies):
+    try:
+        conn = mariadb.connect(
+        user="root",
+        password="i",
+        host="localhost",
+        database="hubspot_companies")
+    except mariadb.Error as fel:
+        print(f"Error connecting to MariaDB: {fel}")
+        sys.exit(1)
+
+    cur = conn.cursor()
+
+    cur.execute("CREATE DATABASE IF NOT EXISTS hubspot_companies")
+    cur.execute("USE hubspot_companies")
+    cur.execute("CREATE TABLE IF NOT EXISTS companies (id BIGINT PRIMARY KEY, name VARCHAR(255), domain VARCHAR(255))")
+
+    for company in hubspot_companies:
+        id = int(company["id"])
+        domain = company["properties"]["domain"]
+        name = company["properties"]["name"]
+    
+        cur.execute("INSERT INTO companies (id, name, domain) VALUES (?, ?, ?)", (id, name, domain))
+
+    conn.commit()
+    conn.close()
 
 def import_hubspot_kunder(hubspot_contacts):
     try:
@@ -18,7 +85,7 @@ def import_hubspot_kunder(hubspot_contacts):
 
     cur.execute("CREATE DATABASE IF NOT EXISTS hubspot_contacts")
     cur.execute("USE hubspot_contacts")
-    cur.execute("CREATE TABLE IF NOT EXISTS customers (id INT PRIMARY KEY, email VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255))")
+    cur.execute("CREATE TABLE IF NOT EXISTS customers (id INT PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), email VARCHAR(255))")
 
     for kund in hubspot_contacts:
         id = int(kund["id"])
@@ -26,11 +93,13 @@ def import_hubspot_kunder(hubspot_contacts):
         firstname = kund["properties"]["firstname"]
         lastname = kund["properties"]["lastname"]
 
-        cur.execute("INSERT INTO customers (id, email, firstname, lastname) VALUES (?, ?, ?, ?)", (id, email, firstname, lastname))
+        cur.execute("INSERT INTO customers (id, email, firstname, lastname) VALUES (?, ?, ?, ?)", (id, firstname, lastname, email))
 
     conn.commit()
     conn.close()
 
+
+# Newsletters ---------------------------------------------------------
 
 def import_newsletters_data(newsletters_data):
     try:
@@ -56,13 +125,13 @@ def import_newsletters_data(newsletters_data):
         
         for kund in newsletters_data:
             cur.execute("""
-                INSERT INTO customers (Id, Email, FirstName, LastName, Country, Zip, Street)
+                INSERT INTO customers (Id, FirstName, LastName, Email, Country, Zip, Street)
                 VALUES (%s, %s, %s, %s, %s, %s, %s)
             """, (
                 kund["Id"],
-                kund["Email"],
                 kund["FirstName"],
                 kund["LastName"],
+                kund["Email"],
                 kund["Country"],
                 kund["Zip"],
                 kund["Street"]
@@ -77,6 +146,42 @@ def import_newsletters_data(newsletters_data):
     finally:
         if conn:
             conn.close()
+
+
+# Zeep ----------------------------------------------
+
+def import_credits(credits_data):
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password="i",
+            host="localhost",
+            database="credits_data")
+
+        cur = conn.cursor()
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS credits (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                customer_number VARCHAR(255),
+                amount INT,
+                currency VARCHAR(255)
+            )
+        """)
+
+        for credit in credits_data:
+            cur.execute("""
+                INSERT INTO credits (customer_number, amount, currency)
+                VALUES (?, ?, ?)
+            """, (credit["customerNumber"], int(credit["amount"]), credit["currency"]))
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    except mariadb.Error as fel:
+        print(f"Ett fel uppstog: {fel}")
+        sys.exit(1)
 
 def import_payments(payments_data):
     try:
@@ -103,7 +208,7 @@ def import_payments(payments_data):
             cur.execute("""
                 INSERT INTO payments (order_number, amount, currency, method, status)
                 VALUES (?, ?, ?, ?, ?)
-            """, (payment["orderNumber"], payment["amount"], payment["currency"], payment["method"], payment["status"]))
+            """, (payment["orderNumber"], int(payment["amount"]), payment["currency"], payment["method"], payment["status"]))
 
         conn.commit()
         cur.close()
@@ -115,7 +220,9 @@ def import_payments(payments_data):
 
 
 if __name__ == "__main__":
-    import_newsletters_data(få_newsletters_kunder())
+    import_hubspot_deals(få_hubspot_deals())
+    import_hubspot_companies(få_hubspot_companies())
     import_hubspot_kunder(få_hubspot_kunder())
+    import_newsletters_data(få_newsletters_kunder())
+    import_credits(få_credits_kunder(customer_numbers=["10001", "10002", "10003", "10004", "330495", "10005", "330496", "10006", "123456", "10007","330497", "10008", "330498", "999999"]))
     import_payments((få_payments(order_numbers=["330495", "330496", "123456", "330497", "330498", "999999"])))
-
